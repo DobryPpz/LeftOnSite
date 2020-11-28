9d610412e29f1b1a636232058970bc791d59aacf
DobryPpz <xaleksx960@gmail.com>
2020-11-28 23:00:09 +0100

    kontynuacja prac nad chunkami

--- pliki gry/src/Chunk.gd
+++ pliki gry/src/Chunk.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
 var nr = Array()
+onready var size_x = get_viewport().size.x
+onready var size_y = get_viewport().size.y
+onready var path_granica_1 = get_tree().get_root().get_node("Gra").get_node("Gracz").get_node("granica_1")
+onready var path_granica_2 = get_tree().get_root().get_node("Gra").get_node("Gracz").get_node("granica_2")
+onready var path_granica_3 = get_tree().get_root().get_node("Gra").get_node("Gracz").get_node("granica_3")
+onready var path_granica_4 = get_tree().get_root().get_node("Gra").get_node("Gracz").get_node("granica_4")
+onready var path_chunki = get_tree().get_root().get_node("Gra").get_node("chunki")
 
 func _ready():
 	pass 
@@ -9,12 +16,43 @@ func gen():
 	pass
 
 func _on_strefa_area_entered(area):
-	#gen()
-	if(not([nr[0],nr[1]-1] in auto_load.nr_chunk)):
-		pass
-	if(not([nr[0],nr[1]+1] in auto_load.nr_chunk)):
-		pass
-	if(not([nr[0]-1,nr[1]] in auto_load.nr_chunk)):
-		pass
-	if(not([nr[0]+1,nr[1]] in auto_load.nr_chunk)):
-		pass
+	if(area==path_granica_1 or area==path_granica_2 or area==path_granica_3 or area==path_granica_4):
+		gen()
+		var scena_chunk = load("res://src/Chunk.tscn")
+		if(not([nr[0],nr[1]-1] in auto_load.nr_chunk)):
+			auto_load.nr_chunk.append([nr[0],nr[1]-1])
+			var node = scena_chunk.instance()
+			node.nr = [nr[0],nr[1]-1]
+			node.position = Vector2(self.position.x,self.position.y-size_y)
+			path_chunki.add_child(node)
+			print("liczba chunków: ",auto_load.nr_chunk.size())
+		if(not([nr[0],nr[1]+1] in auto_load.nr_chunk)):
+			auto_load.nr_chunk.append([nr[0],nr[1]+1])
+			var node = scena_chunk.instance()
+			node.nr = [nr[0],nr[1]+1]
+			node.position = Vector2(self.position.x,self.position.y+size_y)
+			path_chunki.add_child(node)
+			print("liczba chunków: ",auto_load.nr_chunk.size())
+		if(not([nr[0]-1,nr[1]] in auto_load.nr_chunk)):
+			auto_load.nr_chunk.append([nr[0]-1,nr[1]])
+			var node = scena_chunk.instance()
+			node.nr = [nr[0]-1,nr[1]]
+			node.position = Vector2(self.position.x-size_x,self.position.y)
+			path_chunki.add_child(node)
+			print("liczba chunków: ",auto_load.nr_chunk.size())
+		if(not([nr[0]+1,nr[1]] in auto_load.nr_chunk)):
+			auto_load.nr_chunk.append([nr[0]+1,nr[1]])
+			var node = scena_chunk.instance()
+			node.nr = [nr[0]+1,nr[1]]
+			node.position = Vector2(self.position.x+size_x,self.position.y)
+			path_chunki.add_child(node)
+			print("liczba chunków: ",auto_load.nr_chunk.size())
+			
+			
+			
+			
+			
+			
+			
+
+
--- pliki gry/src/chunki.gd
+++ pliki gry/src/chunki.gd
@@ -6,14 +6,21 @@ func _ready():
 	get_node("Chunk_up").position = get_node("Chunk_up").position + Vector2(0,-get_viewport().size.y)
 	get_node("Chunk_up").nr = [0,-1]
 	auto_load.nr_chunk.append([0,-1])
+	
 	get_node("Chunk_down").position = get_node("Chunk_down").position + Vector2(0,get_viewport().size.y)
 	get_node("Chunk_down").nr = [0,1]
 	auto_load.nr_chunk.append([0,1])
+	
 	get_node("Chunk_left").position = get_node("Chunk_left").position + Vector2(-get_viewport().size.x,0)
 	get_node("Chunk_left").nr = [-1,0]
 	auto_load.nr_chunk.append([-1,0])
+	
 	get_node("Chunk_right").position = get_node("Chunk_right").position + Vector2(get_viewport().size.x,0)
 	get_node("Chunk_right").nr = [1,0]
 	auto_load.nr_chunk.append([1,0])
+	
+	auto_load.nr_chunk.append([0,0])
+	
+	
 	
 	
